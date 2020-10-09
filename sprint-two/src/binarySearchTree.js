16afa172f297a923369c801c0cb9a33a04ede210
Linh Dang <linh.dang01@gmail.com>
2020-10-09 16:14:38 -0700

    Working on binary search tree implementation

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,46 @@
-var BinarySearchTree = function(value) {
-};
+class Node {
+  constructor(val) {
+    this.right = null;
+    this.left = null;
+    this.val = val; //val?
+  }
+}
+
+class BinarySearchTree {
+  constructor(val) {
+    this.root = new Node(val); //new Node(root)?
+  }
+
+  insert(val) {
+    var newNode = new Node(val);
+    var helper = function(node) {
+      if (newNode.value < node.value) {
+        if (node.left === null) {
+          node.left = newNode;
+        } else {
+          helper(node.left);
+        }
+      } else {
+        if (node.right === null) {
+          node.right = newNode;
+        } else {
+          helper(node.right);
+        }
+      }
+    };
+    helper(this.root);
+  }
+
+  contains() {
+  }
+
+  depthFirstLog() {
+  }
+}
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
