8eed5b1d0c4986d48731da40da841aae90a38620
Linh Dang <linh.dang01@gmail.com>
2020-10-09 20:38:49 -0700

    Completed binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,46 +1,76 @@
-class Node {
-  constructor(val) {
-    this.right = null;
-    this.left = null;
-    this.val = val; //val?
-  }
-}
+var Node = function(value) {
+  var node = {};
+  node.right = null;
+  node.left = null;
+  node.value = value;
+  return node;
+};
 
-class BinarySearchTree {
-  constructor(val) {
-    this.root = new Node(val); //new Node(root)?
-  }
+var BinarySearchTree = function(value) {
+  var bst = {};
+  bst.root = Node(value);
+  _.extend(bst, bstMethods);
+  return bst;
+};
 
-  insert(val) {
-    var newNode = new Node(val);
-    var helper = function(node) {
-      if (newNode.value < node.value) {
-        if (node.left === null) {
-          node.left = newNode;
-        } else {
-          helper(node.left);
-        }
+bstMethods = {};
+
+bstMethods.insert = function(value) {
+  var newNode = Node(value);
+  var helper = function(node) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
       } else {
-        if (node.right === null) {
-          node.right = newNode;
-        } else {
-          helper(node.right);
-        }
+        helper(node.left);
       }
-    };
-    helper(this.root);
-  }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        helper(node.right);
+      }
+    }
+  };
+  helper(this.root);
+};
 
-  contains() {
-  }
+bstMethods.contains = function(target) {
+  var helper = function(node) {
+    if (node.value === target) {
+      return true;
+    }
+    if (node.left === null && node.right === null) {
+      return false;
+    }
+    if (target < node.value) {
+      return helper(node.left);
+    } else {
+      return helper(node.right);
+    }
+  };
+  return helper(this.root);
+};
 
-  depthFirstLog() {
-  }
-}
+bstMethods.depthFirstLog = function(callback) {
+  var helper = function(node) {
+    callback(node.value);
+    if (node.left) {
+      helper(node.left);
+    }
+    if (node.right) {
+      helper(node.right);
+    }
+  };
+  helper(this.root);
+};
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert - O(log(n))
+ * contains - O(log(n))
+ * depthFirstLog - O(n)
  */
 
 
