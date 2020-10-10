c97346fb2559943029ba377e11d9e29eef1fbdc4
Linh Dang <linh.dang01@gmail.com>
2020-10-09 20:48:47 -0700

    Added some pseudocode for the functions

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = null; // fix me // collection
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // use the item as the key and the value
+  // add to the set._storage collection
 };
 
 setPrototype.contains = function(item) {
+  // if the value at the collection using item as the key
+  // return false
+  // else
+  // return true
 };
 
 setPrototype.remove = function(item) {
+  // if item is in the set
+  // remove item at the given item location
+  // else
+  // return undefined
 };
 
 /*
