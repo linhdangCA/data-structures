ff1c2687148c01f95925a3261134631564ffd4e2
Linh Dang <linh.dang01@gmail.com>
2020-10-10 12:11:34 -0700

    Completed bare minimums for graph, hashTable, and set

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,67 @@
-
-
-// Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
 };
 
-// Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
+Graph.prototype.addNode = function(newNode) {
+  this.nodes[newNode] = {};
+  this.nodes[newNode].hasEdgeTo = {};
 };
 
-// Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
+Graph.prototype.contains = function(target) {
+  if (this.nodes[target]) {
+    return true;
+  }
+  return false;
 };
 
-// Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var keys in this.nodes[node].hasEdgeTo) {
+    this.removeEdge(keys, this.nodes[node].hasEdgeTo);
+    this.removeEdge(this.nodes[node].hasEdgeTo, keys);
+  }
+  delete this.nodes[node];
 };
 
-// Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (this.nodes[toNode] === undefined || this.nodes[fromNode] === undefined) {
+    return false;
+  }
+  if (this.nodes[toNode].hasEdgeTo[fromNode] === toNode) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-// Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  if (this.nodes[fromNode] !== undefined && this.nodes[toNode] !== undefined) {
+    this.nodes[fromNode].hasEdgeTo[toNode] = fromNode;
+    this.nodes[toNode].hasEdgeTo[fromNode] = toNode;
+  }
 };
 
-// Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  if (this.nodes[fromNode] !== undefined) {
+    this.nodes[fromNode].hasEdgeTo[toNode] = null;
+  }
+  if (this.nodes[toNode] !== undefined) {
+    this.nodes[toNode].hasEdgeTo[fromNode] = null;
+  }
 };
 
-// Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var nodes in this.nodes) {
+    cb(nodes);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addNode - O(1)
+ * contains - O(1)
+ * removeNode - O(1)
+ * hasEdge - O(1)
+ * addEdge - O(1)
+ * removeEdge - O(1)
+ * forEachNode - O(n)
  */
-
-
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,30 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me // collection
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  // use the item as the key and the value
-  // add to the set._storage collection
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
-  // if the value at the collection using item as the key
-  // return false
-  // else
-  // return true
+  if (this._storage[item] !== item) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
 setPrototype.remove = function(item) {
-  // if item is in the set
-  // remove item at the given item location
-  // else
-  // return undefined
+  if (this._storage[item] === item) {
+    delete this._storage[item];
+  } else {
+    return undefined;
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * always constant - O(1)
  */
